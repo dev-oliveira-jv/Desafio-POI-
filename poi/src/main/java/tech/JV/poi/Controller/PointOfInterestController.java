b29aa8658f3cb6020cadf0d3d9edf6128adcba27
João <joaovictorro102@gmail.com>
2026-01-19 16:52:55 -0400

    Feat(Controller POI): adicionando o endpoint de listagem de Pontos de interrese

--- poi/src/main/java/tech/JV/poi/Controller/PointOfInterestController.java
+++ poi/src/main/java/tech/JV/poi/Controller/PointOfInterestController.java
@@ -1,8 +1,13 @@
 package tech.JV.poi.Controller;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import tech.JV.poi.Controller.dtos.CreatePOI;
@@ -25,5 +30,14 @@ public class PointOfInterestController {
         repository.save(new PointOfInterest( body.name(), body.x(), body.y()));
 
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/points-of-interests-list")
+    public ResponseEntity<Page<PointOfInterest>> listPoi(@RequestParam(name = "page", defaultValue = "0") Integer page,
+                                        @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize){
+
+        var allPoi = repository.findAll(PageRequest.of(page, pageSize));
+
+        return ResponseEntity.ok(allPoi);
     }
 }
