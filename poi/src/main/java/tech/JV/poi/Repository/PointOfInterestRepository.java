a7e2363099bef4083d39d18cdce2802dfe7b9bc3
João <joaovictorro102@gmail.com>
2026-01-19 17:39:04 -0400

    Feat(Repository POI): Implementação da query personalizada para o end point de calculo de localização

--- poi/src/main/java/tech/JV/poi/Repository/PointOfInterestRepository.java
+++ poi/src/main/java/tech/JV/poi/Repository/PointOfInterestRepository.java
@@ -1,10 +1,23 @@
 package tech.JV.poi.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import tech.JV.poi.Entity.PointOfInterest;
 
 public interface PointOfInterestRepository extends JpaRepository<PointOfInterest, Long> {
+
+    @Query("""
+            SELECT p FROM PointOfInterest 
+            WHERE (p.x >= :xMin AND p.x <= :xMax AND p.y >= :yMin AND p.y <= :yMax)
+            """)
+    List<PointOfInterest> findNearMe(@Param("xMin") long xMin,
+                                     @Param("xMax") long xMax,
+                                     @Param("yMax") long yMax,
+                                     @Param("yMin") long yMin);
 
     
 }
